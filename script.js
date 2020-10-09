e8e592e572846213322f66982a7e11865c07cdb0
SaltyBr <SaltyBrain@yandex.ru>
2020-10-09 12:40:25 +0300

    main task

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit,
-    mission, 
-    period;
+let money = 300000, 
+    income = 'Freelance 120000', 
+    addExpenses = 'Rent, Food, Transport', 
+    deposit = true,
+    mission = 1000000, 
+    period = 6,
+    budgetDay = money/30;
 
-alert('Hello World!');
-
-console.log('Hello World!');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Period is equal ' + period + ' months, Mission is to get ' + mission + ' dollars');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
