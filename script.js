5cbb1ef8f0417e1dbcbac5c4452a907437762e8e
SaltyBr <SaltyBrain@yandex.ru>
2020-10-10 11:48:59 +0300

    make less code strings

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 let num = 266219;
 let multi = 1;
-let str = String(num);
-
-let arr = str.split('');
-
+let arr = String(num).split('');
 
 for (let i = 0; i<arr.length; i++) {
     arr[i] = +arr[i];
 }
 
 console.log(arr);
+
 for (let i = 0; i<arr.length; i++) {
     multi *= arr[i];
 }
@@ -20,8 +18,6 @@ let power = multi**3;
 
 console.log(power);
 
-let strPower = String(power);
-
-let arrPower = strPower.split('');
+let arrPower = String(power).split('');
 
 console.log(arrPower[0],arrPower[1]);
