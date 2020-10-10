9c4e403430d299e5f20090d56431d79026861556
SaltyBr <SaltyBrain@yandex.ru>
2020-10-10 11:46:16 +0300

    add task done

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit,
-    mission, 
-    period;
+let num = 266219;
+let multi = 1;
+let str = String(num);
 
-alert('Hello World!');
+let arr = str.split('');
 
-console.log('Hello World!');
+
+for (let i = 0; i<arr.length; i++) {
+    arr[i] = +arr[i];
+}
+
+console.log(arr);
+for (let i = 0; i<arr.length; i++) {
+    multi *= arr[i];
+}
+
+console.log(multi);
+
+let power = multi**3;
+
+console.log(power);
+
+let strPower = String(power);
+
+let arrPower = strPower.split('');
+
+console.log(arrPower[0],arrPower[1]);
