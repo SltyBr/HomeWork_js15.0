d8fede9bdccde40baaeb65046bc0d9741ebd0363
SaltyBr <SaltyBrain@yandex.ru>
2020-10-14 09:58:34 +0300

    update script.js

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ if (str !== String(str))
 {
     console.log('это не строка');
 } else {
-    let str1 = '' + str.trim();
-    if (str1.length > 30) {console.log(str1.substr(0, 30) + '...');}
+    let str1 = str.trim();
+    if (str1.length > 30) {console.log(str1.substr(0, 30) + '...');} 
+    else {console.log(str1 + ': строка содержит меньше 30 символов, операция невозможна');}
 }}
 
-strTrim('                adasdasdasaaaaaaaaaaaaaaa      adsdasdasd                      ');
+strTrim('                aaaaaaaaaaaa                      ');
