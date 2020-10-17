11278a4f0d10b7d4f754ae1dd1c60b651097f91c
SaltyBr <SaltyBrain@yandex.ru>
2020-10-17 10:22:53 +0300

    lesson06 done

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 'use strict';
 
-let secret = 47;
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let secret = Math.ceil(Math.random()*101);
+let n = 10;
  
 function guess() {
-    let num = prompt('Угадай число от 1 до 100');
-    if (num == secret) {
-        alert('Угадали!');
-    } else if (num === null) {
-        alert('Игра окончена');
-    }
-    else if (num > secret) {
-        alert('Загаданное число меньше');
-        guess();}
-     else if (num < secret) {
-        alert('Загаданное число больше');
-        guess();
-    } else if (isNaN(num)) {
-        alert('Это не число');
-        guess();
+    let num = prompt('Угадай число от 1 до 100' + ', у вас есть ' + n + ' попыток');
+    n--;
+    if (n == 0) 
+    {
+        if (confirm('Игра окончена, хотите попробовать ещё?')) 
+            {
+            n = 10;
+            guess();
+            }
+    } else {
+        if (num == secret) {
+            alert('Угадали!');
+        }   
+        else if (!isNumber(num)) {
+            alert('Введи число!');
+            guess();
+        }   
+        else if (num === null) {
+            alert('Игра окончена');
+        }
+        else if (num > secret) {
+            alert('Загаданное число меньше');
+            guess();
+        }
+        else if (num < secret) {
+            alert('Загаданное число больше');
+            guess();
+        }
     }
 }   
 
 guess();
+
