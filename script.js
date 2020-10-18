5ec146d0814b0f491b78c9a19711690df371530c
SaltyBr <SaltyBrain@yandex.ru>
2020-10-18 18:37:30 +0300

    lesson07 add task done

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit,
-    mission, 
-    period;
+let body = document.querySelector('.week'),
+    week = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'],
+    now = new Date();
 
-alert('Hello World!');
+function weekWindow()
+{
+    for (let i=0; i< week.length; i++) {
+        body.innerHTML += `<div>${week[i]}</div>`;
+    }
+    return body.innerHTML;
+}
 
-console.log('Hello World!');
+for (let i=0; i< week.length; i++) {
+    if (i == now.getDay()) {
+        week[i] = '<b>' + week[i] + '</b>';
+    }
+    if (i == 0) {
+        week[i] = '<i>' + week[i] + '</i>';
+    }
+    if (i == 6) {
+        week[i] = '<i>' + week[i] + '</i>';
+    }
+}
+
+console.log(week);
+
+weekWindow();
