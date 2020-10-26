27ef2750a1bf7ca814c000b9c244b59591ce3c93
SaltyBr <SaltyBrain@yandex.ru>
2020-10-26 18:46:10 +0300

    make main task lesson12

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
-let money, 
-    income, 
-    addExpenses, 
-    deposit,
-    mission, 
-    period;
+'use sctrict';
 
-alert('Hello World!');
+const  todoControl = document.querySelector('.todo-control'),
+    btnAddItem = document.getElementById('add'),
+    headerInput = document.querySelector('.header-input'),
+    todoList = document.querySelector('.todo-list'),
+    todoCompleted = document.querySelector('.todo-completed');
 
-console.log('Hello World!');
+const todoData = [];
+
+const render = function() {
+
+    todoList.textContent = '';
+    todoCompleted.textContent = '';
+
+    todoData.forEach(function(item){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+        '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+        '</div>';
+
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+
+        const todoComplete = li.querySelector('.todo-complete');
+
+        todoComplete.addEventListener('click', function(){
+            item.completed = !item.completed;
+            render();
+        });
+    });
+};
+
+todoControl.addEventListener('submit', function(event){
+
+    event.preventDefault();
+
+    const newTodo = {
+        value: headerInput.value,
+        completed: false
+    };
+
+    todoData.push(newTodo);
+
+    render();
+
+    todoControl.querySelector('.header-input').value = null;
+});
+
+render();
+
+btnAddItem.setAttribute('disabled', 'disabled');
+
+console.dir(btnAddItem.disabled);
